50f8ce1d23a93b265df50a936c8071f8affce782
kenleeyx <kenleeyx@gmail.com>
2023-12-13 22:27:17 +0800

    final commit

--- src/Components/SearchPage/CategoryBlock.js
+++ src/Components/SearchPage/CategoryBlock.js
@@ -5,6 +5,7 @@ import axios from "axios";
 
 export const CategoryBlock = ({ searchMode, category, filterCriteria, setFilterCriteria }) => {
     const [options, setOptions] = useState([])
+    const [inputText, setInputText] = useState('')
 
     useEffect(() => {
         const getOptionsFromDatabase = async () => {
@@ -21,7 +22,8 @@ export const CategoryBlock = ({ searchMode, category, filterCriteria, setFilterC
             qualifications: ['Self Taught', 'Low Grade(eg ABRSM 1-5)', 'High Grade(eg ABRSM 6-8)','Diploma', "Bachelor's degree", "Master's degree", "Doctorate"],
             musicianship: ['Amateur', 'Full-time music student', 'Semi-Pro', 'Professional', 'Educator' ],
             musicianshipGroup: ['Amateur', 'Semi-Pro', 'Professional', 'School' ],
-            'ensemble type': ['Band', 'Orchestra', 'Choir', 'Concert Band', 'Marching Band', 'Chamber Group']
+            'ensemble type': ['Band', 'Orchestra', 'Choir', 'Concert Band', 'Marching Band', 'Chamber Group'],
+            name:''
         }
         if (category === 'Musicianship' && searchMode === 'groups') {
             setOptions(nonDatabaseOptions['musicianshipGroup'])
@@ -35,6 +37,9 @@ export const CategoryBlock = ({ searchMode, category, filterCriteria, setFilterC
     return (
         <div className='flex flex-row w-full items-center'>
             <h1 className='text-xl font-bold w-[6em]'>{category}</h1>
+            {console.log(category)}
+            {console.log(filterCriteria[category])}
+            {options ? 
             <Select
                 className='w-[10em] mx-[1em]'
                 defaultValue={{ value: category, label: category }}
@@ -48,7 +53,21 @@ export const CategoryBlock = ({ searchMode, category, filterCriteria, setFilterC
                         return { ...prevState, [category]: e.value }
                     })
                 }}
-            />
+            /> :
+            <input
+            className = 'w-[10em] mx-[1em]'
+            type="text"
+            id= {`input-${category}`}
+            placeholder = "Enter name"
+            value = {inputText}
+            onChange={(e)=>{
+                setInputText(e.target.value)
+                setFilterCriteria((prevState)=>{
+                    return { ...prevState, [category]: e.target.value }
+                })
+            }}>
+            </input>
+            }
             <button onClick={() => setFilterCriteria((prevState) => {
                 const newState = { ...prevState };
                 delete newState[category];
